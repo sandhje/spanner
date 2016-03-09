480f30d8b9de91b093b6e67955c087f0ea9fdecf
Sandhje <sandhje@gmail.com>
2016-03-10 00:04:53 +0100

    Allow merging of multiple arrays

--- src/Adapter/BaseAdapter.php
+++ src/Adapter/BaseAdapter.php
@@ -8,17 +8,19 @@ namespace Sandhje\Spanner\Adapter;
  */
 class BaseAdapter
 {
-    protected function mergeConfig($baseConfig, $environmentConfig)
+    
+    protected function mergeConfig()
     {
-        if(is_array($baseConfig) && is_array($environmentConfig)) {
-            return array_replace_recursive($baseConfig, $environmentConfig);
-        } else if(is_array($environmentConfig)) {
-            return $environmentConfig;
-        } else if(is_array($baseConfig)) {
-            return $baseConfig;
-        } else {
-            return false;
+        $result = array();
+        
+        $arg_list = func_get_args();
+        for ($i = 0; $i < func_num_args(); $i++) {
+            if(is_array($arg_list[$i])) {
+                $result = array_replace_recursive($result, $arg_list[$i]);
+            }
         }
+        
+        return $result;
     }
 }
 
