73e9697450955b03d1984bada89890add93c88dd
Sandhje <sandhje@gmail.com>
2016-03-10 22:30:47 +0100

    Implement unit tests

--- tests/Adapter/ArrayAdapterTest.php
+++ tests/Adapter/ArrayAdapterTest.php
@@ -65,4 +65,64 @@ class ArrayAdapterTest extends \PHPUnit_Framework_TestCase
         $resultConfig = array("a"=>"b","c"=>"d");
         $this->assertEquals($resultConfig, $result);
     }
+    
+    public function testMergeRegions()
+    {
+        // Arrange
+        $path1 = "/foo";
+        $path2 = "/bar";
+        $region = "acme";
+        $file1 = $path1 . "/" . $region . ".php";
+        $file2 = $path2 . "/" . $region . ".php";
+        $array1 = array("a" => "lorem", "b" => "ipsum");
+        $array2 = array("b" => "dolor", "c" => "sit amet");
+        $expected = array("a" => "lorem", "b" => "dolor", "c" => "sit amet");
+        $config = new Config();
+        $config->appendPath($path1);
+        $config->appendPath($path2);
+        $filesystem = Mockery::mock('Sandhje\Spanner\Filesystem\Filesystem');
+        $filesystem->shouldReceive('is_file')->with($file1)->andReturn(true);
+        $filesystem->shouldReceive('is_readable')->with($file1)->andReturn(true);
+        $filesystem->shouldReceive('load')->with($file1)->andReturn($array1);
+        $filesystem->shouldReceive('is_file')->with($file2)->andReturn(true);
+        $filesystem->shouldReceive('is_readable')->with($file2)->andReturn(true);
+        $filesystem->shouldReceive('load')->with($file2)->andReturn($array2);
+    
+        // Act
+        $arrayAdapter = new ArrayAdapter($filesystem);
+        $result = $arrayAdapter->load($config, $region);
+    
+        // Assert
+        $this->assertEquals($expected, $result);
+    }
+    
+    public function testMergeRegionsRecursive()
+    {
+        // Arrange
+        $path1 = "/foo";
+        $path2 = "/bar";
+        $region = "acme";
+        $file1 = $path1 . "/" . $region . ".php";
+        $file2 = $path2 . "/" . $region . ".php";
+        $array1 = array("a" => array("b" => "lorem", "c" => "ipsum"));
+        $array2 = array("a" => array("c" => "dolor", "d" => "sit amet"));
+        $expected = array("a" => array("b" => "lorem", "c" => "dolor", "d" => "sit amet"));
+        $config = new Config();
+        $config->appendPath($path1);
+        $config->appendPath($path2);
+        $filesystem = Mockery::mock('Sandhje\Spanner\Filesystem\Filesystem');
+        $filesystem->shouldReceive('is_file')->with($file1)->andReturn(true);
+        $filesystem->shouldReceive('is_readable')->with($file1)->andReturn(true);
+        $filesystem->shouldReceive('load')->with($file1)->andReturn($array1);
+        $filesystem->shouldReceive('is_file')->with($file2)->andReturn(true);
+        $filesystem->shouldReceive('is_readable')->with($file2)->andReturn(true);
+        $filesystem->shouldReceive('load')->with($file2)->andReturn($array2);
+    
+        // Act
+        $arrayAdapter = new ArrayAdapter($filesystem);
+        $result = $arrayAdapter->load($config, $region);
+    
+        // Assert
+        $this->assertEquals($expected, $result);
+    }
 }
--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -2,66 +2,105 @@
 
 namespace Sandhje\Spanner\Test;
 
+use Sandhje\Spanner\Config;
+use Mockery;
+
 class ConfigTest extends \PHPUnit_Framework_TestCase
 {
+    public function tearDown()
+    {
+        Mockery::close();
+    }
     
     public function testSetGetPathArray()
     {
         // Arrange
+        $pathArray = array("foo/", "bar/");
+        $config = new Config();
     
         // Act
+        $config->setPathArray($pathArray);
+        $resultPathArray = $config->getPathArray();
     
         // Assert
-        $this->assertTrue(false);
+        $this->assertEquals($pathArray, $resultPathArray);
     }
     
     public function testAppendPath()
     {
         // Arrange
+        $pathArray = array("foo/", "bar/");
+        $config = new Config();
         
         // Act
+        $config->appendPath($pathArray[0]);
+        $config->appendPath($pathArray[1]);
+        $resultPathArray = $config->getPathArray();
         
         // Assert
-        $this->assertTrue(false);
+        $this->assertEquals($pathArray, $resultPathArray);
     }
     
     public function testPrependPath()
     {
         // Arrange
+        $pathArray = array("foo/", "bar/");
+        $config = new Config();
     
         // Act
+        $config->prependPath($pathArray[1]);
+        $config->prependPath($pathArray[0]);
+        $resultPathArray = $config->getPathArray();
     
         // Assert
-        $this->assertTrue(false);
+        $this->assertEquals($pathArray, $resultPathArray);
     }
     
     public function testSetGetEnvironment()
     {
         // Arrange
+        $environment = "foo";
+        $config = new Config();
     
         // Act
+        $config->setEnvironment($environment);
+        $resultEnvironment = $config->getEnvironment();
     
         // Assert
-        $this->assertTrue(false);
+        $this->assertEquals($environment, $resultEnvironment);
     }
     
     public function testGet()
     {
         // Arrange
+        $region = "acme";
+        $regionArray = array("foo" => "bar");
+        $arrayAdapter = Mockery::mock('Sandhje\Spanner\Adapter\ArrayAdapter');
+        $arrayAdapter->shouldReceive("load")->with(Mockery::type('Sandhje\Spanner\Config'),$region)->andReturn($regionArray);
+        $config = new Config($arrayAdapter);
     
         // Act
+        $result = $config->get($region);
     
         // Assert
-        $this->assertTrue(false);
+        $this->assertEquals($regionArray, $result);
     }
     
     public function testSet()
     {
         // Arrange
+        $region = "acme";
+        $regionArray = array("foo" => "bar");
+        $regionArray2 = array("foo" => "lorem");
+        $arrayAdapter = Mockery::mock('Sandhje\Spanner\Adapter\ArrayAdapter');
+        $arrayAdapter->shouldReceive("load")->with(Mockery::type('Sandhje\Spanner\Config'),$region)->andReturn($regionArray);
+        $config = new Config($arrayAdapter);
     
         // Act
+        $config->set($region, key($regionArray2), current($regionArray2));
+        $result = $config->get($region);
     
         // Assert
-        $this->assertTrue(false);
+        $this->assertEquals($regionArray2, $result);
     }
 }
